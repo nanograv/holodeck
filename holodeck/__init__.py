c361d35044ade9406869fdba2a7e6f8adfd5079f
Luke Zoltan Kelley <lzkelley@gmail.com>
2021-02-08 09:14:27 -0700

    Add scipy and astropy requirements, initialize cosmology object in root package

--- holodeck/__init__.py
+++ holodeck/__init__.py
@@ -3,8 +3,15 @@ holodeck
 Supermassive binary black hole simulator for pulsar timing array signals and galaxy population  statistics.
 """
 
+import astropy
+import astropy.cosmology
+cosmo = astropy.cosmology.WMAP9
+del astropy
+
 from . import utils     # noqa
 from . import holodeck  # noqa
+from . import observations # noqa
+
 
 # Handle versioneer
 from ._version import get_versions
