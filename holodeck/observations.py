e1c20e9df922d564169cee009194e74412c258b8
Luke Zoltan Kelley <lzkelley@gmail.com>
2021-08-19 12:23:54 -0500

    Add some Stellar-mass--Halo-mass relations, start adding some density profiles

--- holodeck/observations.py
+++ holodeck/observations.py
@@ -1,9 +1,39 @@
 """
+
+References
+----------
+* [NFW-97] : Navarro, Frenk & White 1997
+    A Universal Density Profile from Hierarchical Clustering
+    https://ui.adsabs.harvard.edu/abs/1997ApJ...490..493N/abstract
+
+* [Guo-2010] : Guo, White, Li & Boylan-Kolchin 2010
+    How do galaxies populate dark matter haloes?
+    https://ui.adsabs.harvard.edu/abs/2010MNRAS.404.1111G/abstract
+
+* [Kormendy+2013] : Kormendy & Ho 2013
+    Coevolution (Or Not) of Supermassive Black Holes and Host Galaxies
+    https://ui.adsabs.harvard.edu/abs/2013ARA%26A..51..511K/abstract
+
+* [McConnell+2013] : McConnell & Ma 2013
+    Revisiting the Scaling Relations of Black Hole Masses and Host Galaxy Properties
+    https://ui.adsabs.harvard.edu/abs/2013ApJ...764..184M/abstract
+
+* [Behroozi+2013] : Behroozi, Wechsler & Conroy 2013
+    The Average Star Formation Histories of Galaxies in Dark Matter Halos from z = 0-8
+    https://ui.adsabs.harvard.edu/abs/2013ApJ...770...57B/abstract
+
+* [Klypin+2016] : Klypin et al. 2016
+    MultiDark simulations: the story of dark matter halo concentrations and density profiles
+    https://ui.adsabs.harvard.edu/abs/2016MNRAS.457.4340K/abstract
+
 """
 
-import numpy as np
+import abc
 
-from holodeck import log
+import numpy as np
+import scipy as sp
+
+from holodeck import log, cosmo
 from holodeck.constants import MSOL
 
 
@@ -253,3 +283,248 @@ class Kormendy_Ho_2013(_Galaxy_Blackhole_Relation):
             'y': 1e9 * MSOL,    # MBH units
         },
     }
+
+
+class Klypin_2016:
+    """Interpolate between redshifts and masses to find DM halo concentrations.
+
+    Eq. 24 & Table 2
+    """
+    _redz = [0.00e+00, 3.50e-01, 5.00e-01, 1.00e+00, 1.44e+00,
+             2.15e+00, 2.50e+00, 2.90e+00, 4.10e+00, 5.40e+00]
+    _c0 = [7.40e+00, 6.25e+00, 5.65e+00, 4.30e+00, 3.53e+00,
+           2.70e+00, 2.42e+00, 2.20e+00, 1.92e+00, 1.65e+00]
+    _gamma = [1.20e-01, 1.17e-01, 1.15e-01, 1.10e-01, 9.50e-02,
+              8.50e-02, 8.00e-02, 8.00e-02, 8.00e-02, 8.00e-02]
+    _mass0 = [5.50e+05, 1.00e+05, 2.00e+04, 9.00e+02, 3.00e+02,
+              4.20e+01, 1.70e+01, 8.50e+00, 2.00e+00, 3.00e-01]
+
+    _interp = lambda xx, yy: sp.interpolate.interp1d(xx, yy, kind='linear', bounds_error=False, fill_value=np.nan)
+    _zz = np.log10(1 + np.array(_redz))
+    _lin_interp_c0 = _interp(_zz, np.log10(_c0))
+    _lin_interp_gamma = _interp(_zz, np.log10(_gamma))
+    _lin_interp_mass0 = _interp(_zz, np.log10(_mass0)+np.log10(1e12 * MSOL / cosmo.h))
+
+    @classmethod
+    def _c0(cls, redz):
+        xx = np.log10(1 + redz)
+        yy = np.power(10.0, cls._lin_interp_c0(xx))
+        return yy
+
+    @classmethod
+    def _gamma(cls, redz):
+        xx = np.log10(1 + redz)
+        yy = np.power(10.0, cls._lin_interp_gamma(xx))
+        return yy
+
+    @classmethod
+    def _mass0(cls, redz):
+        xx = np.log10(1 + redz)
+        yy = np.power(10.0, cls._lin_interp_mass0(xx))
+        return yy
+
+    @classmethod
+    def concentration(cls, mass, redz):
+        c0 = cls._c0(redz)
+        gamma = cls._gamma(redz)
+        mass0 = cls._mass0(redz)
+        f1 = np.power(mass/(1e12*MSOL/cosmo.h), -gamma)
+        f2 = 1 + np.power(mass/mass0, 0.4)
+        conc = c0 * f1 * f2
+        return conc
+
+
+class _Density_Profile(abc.ABC):
+    pass
+
+
+class NFW(_Density_Profile):
+
+    @staticmethod
+    def density(rads, mhalo, redz):
+        """NFW DM Density profile.
+
+        [NFW-97]
+        """
+        # Get Halo concentration
+        conc = Klypin_2016.concentration(mhalo, redz)
+        log_c_term = np.log(1 + conc) - conc/(1+conc)
+
+        # Critical over-density
+        delta_c = (200/3) * (conc**3) / log_c_term
+        # NFW density (*not* the density at the characteristic-radius)
+        rho_s = cosmo.critical_density(redz).cgs.value * delta_c
+        # scale-radius
+        rs = mhalo / (4*np.pi*rho_s*log_c_term)
+        rs = np.power(rs, 1.0/3.0)
+
+        # Calculate NFW profile
+        dens = rads / rs
+        dens = dens * np.square(1 + dens)
+        dens = rho_s / dens
+        return dens
+
+
+# =================================================================================================
+# ====                             Stellar-Mass Halo-Mass Relation                             ====
+# =================================================================================================
+
+
+class _StellarMass_HaloMass(abc.ABC):
+
+    _NUM_GRID = 200
+    _MHALO_GRID_EXTR = [9, 16]
+
+    def __init__(self):
+        mhalo = np.logspace(*self._MHALO_GRID_EXTR, self._NUM_GRID) * MSOL
+        mstar = self.stellar_mass(mhalo)
+        self._mhalo_log = np.log10(mhalo)
+        self._mstar_log = np.log10(mstar)
+        return
+
+    @abc.abstractmethod
+    def stellar_mass(self, mhalo, redz=0.0):
+        pass
+
+    def halo_mass(self, mstar, redz=0.0):
+        if redz != 0.0:
+            raise ValueError("Non-redshift-zero values unsupported!")
+
+        xold = self._mass_grid_star_log
+        yold = self._mass_grid_halo_log
+        ynew = np.interp(np.log10(mstar), xold, yold, left=np.nan, right=np.nan)
+        ynew = np.power(10.0, ynew)
+        return ynew
+
+
+class Guo_2010(_StellarMass_HaloMass):
+    """
+    Guo+2010 : Eq.3
+    https://ui.adsabs.harvard.edu/abs/2010MNRAS.404.1111G/abstract
+    """
+
+    _NORM = 0.129
+    _M0 = (10**11.4) * MSOL
+    _ALPHA = 0.926
+    _BETA = 0.261
+    _GAMMA = 2.440
+
+    @classmethod
+    def stellar_mass(cls, mhalo):
+        M0 = cls._M0
+        t1 = np.power(mhalo/M0, -cls._ALPHA)
+        t2 = np.power(mhalo/M0, +cls._BETA)
+        mstar = mhalo * cls._NORM * np.power(t1 + t2, -cls._GAMMA)
+        return mstar
+
+
+class Behroozi_2013(_StellarMass_HaloMass):
+    """
+    [Behroozi+2013] best fit values are at the beginning of Section 5 (pg.9), uncertainties are 1-sigma
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._f0 = self._f_func(0.0)
+        super().__init__(*args, **kwargs)
+        return
+
+    def _nu_func(sca):
+        """
+        [Behroozi+2013] Eq. 4
+        """
+        return np.exp(-4.0 * sca*sca)
+
+    @classmethod
+    def _param_func(cls, redz, v0, va, vz, va2=None):
+        """
+        [Behroozi+2013] Eq. 4
+        """
+        rv = v0
+        if redz > 0.0:
+            sca = cosmo._z_to_a(redz)
+            rv += cls._nu_func(sca) * (va * (sca - 1.0) + vz * redz)
+            if va2 is not None:
+                rv += va2 * (sca - 1.0)
+        return rv
+
+    @classmethod
+    def _eps(cls, redz=0.0):
+        e0 = -1.777   # +0.133 -0.146
+        ea = -0.006   # +0.113 -0.361
+        ez = 0.0      # +0.003 -0.104
+        ea2 = -0.119  # +0.061 -0.012
+        eps = 10.0 ** cls._param_func(redz, e0, ea, ez, va2=ea2)
+        return eps
+
+    @classmethod
+    def _m1(cls, redz=0.0):
+        m0 = 11.514   # +0.053 -0.009
+        ma = -1.793   # +0.315 -0.330
+        mz = -0.251   # +0.012 -0.125
+        m1 = MSOL * 10.0 ** cls._param_func(redz, m0, ma, mz)
+        return m1
+
+    @classmethod
+    def _alpha(cls, redz=0.0):
+        a0 = -1.412   # +0.020 -0.105
+        aa = 0.731    # +0.344 -0.296
+        az = 0.0
+        alpha = cls._param_func(redz, a0, aa, az)
+        return alpha
+
+    @classmethod
+    def _delta(cls, redz=0.0):
+        d0 = 3.508   # +0.087 -0.369
+        da = 2.608   # +2.446 -1.261
+        dz = -0.043  # +0.958 -0.071
+        delta = cls._param_func(redz, d0, da, dz)
+        return delta
+
+    @classmethod
+    def _gamma(cls, redz=0.0):
+        g0 = 0.316   # +0.076 -0.012
+        ga = 1.319   # +0.584 -0.505
+        gz = 0.279   # +0.256 -0.081
+        gamma = cls._param_func(redz, g0, ga, gz)
+        return gamma
+
+    @classmethod
+    def _xsi(cls, redz=0.0):
+        """
+        [Behroozi+2013] Eq.5
+        """
+        x0 = 0.218   # +0.011 -0.033
+        xa = -0.023  # +0.052 -0.068
+
+        xsi = x0
+        if redz > 0.0:
+            sca = cosmo._z_to_z(redz)
+            xsi += xa * (sca - 1.0)
+
+        return xsi
+
+    @classmethod
+    def _f_func(cls, xx, redz=0.0):
+        """
+        [Behroozi+2013] Eq.3 (lower)
+        """
+        # alpha, delta, gamma
+        alpha = cls._alpha(redz)
+        delta = cls._delta(redz)
+        gamma = cls._gamma(redz)
+
+        t1 = -np.log10(10**(alpha*xx) + 1)
+        t2 = np.log10(1 + np.exp(xx)) ** gamma
+        t3 = 1 + np.exp(10.0 ** -xx)
+        ff = t1 + delta * t2 / t3
+        return ff
+
+    def stellar_mass(self, mhalo, redz=0.0):
+        """
+        [Behroozi+2013] Eq.3 (upper)
+        """
+        eps = self._eps(redz)
+        m1 = self._m1(redz)
+        mstar = np.log10(eps*m1/MSOL) + self._f_func(np.log10(mhalo/m1), redz) - self._f0
+        mstar = np.power(10.0, mstar) * MSOL
+        return mstar
