ab12627dd03ed2b190f3633c8aabc28b4fde9138
Luke Zoltan Kelley <lzkelley@gmail.com>
2021-08-23 14:13:27 -0500

    BUG: fix issue with mass (and derived) calculations for density_profile; BUG: fix interpolants in mstar-mhalo relations

--- holodeck/observations.py
+++ holodeck/observations.py
@@ -91,26 +91,25 @@ class _Galaxy_Blackhole_Relation:
     NORM = {}
     _VALID_RELATIONS = ['vdisp', 'mbulge']
 
-    def __init__(self):
-        return
-
-    def _values(self, relation):
-        if relation not in self._VALID_RELATIONS:
-            err = f"`relation` {relation} must be one of '{self._VALID_RELATIONS}'!"
+    @classmethod
+    def _values(cls, relation):
+        if relation not in cls._VALID_RELATIONS:
+            err = f"`relation` {relation} must be one of '{cls._VALID_RELATIONS}'!"
             raise ValueError(err)
 
-        fits = self.FITS[relation]
+        fits = cls.FITS[relation]
         alpha = fits['alpha']
         beta = fits['beta']
         eps = fits['eps']
 
-        norm = self.NORM[relation]
+        norm = cls.NORM[relation]
         x0 = norm['x']
         y0 = norm['y']
 
         return x0, y0, alpha, beta, eps
 
-    def _parse_scatter(self, scatter):
+    @classmethod
+    def _parse_scatter(cls, scatter):
         if (scatter is False) or (scatter is None):
             scatter = 0.0
         elif scatter is True:
@@ -127,13 +126,16 @@ class _Galaxy_Blackhole_Relation:
 
         return scatter
 
-    def mbh_from_mbulge(self, mbulge, scatter=False):
-        return self._mbh_from_galaxy(mbulge, 'mbulge', forward=True, scatter=scatter)
+    @classmethod
+    def mbh_from_mbulge(cls, mbulge, scatter=False):
+        return cls._mbh_from_galaxy(mbulge, 'mbulge', forward=True, scatter=scatter)
 
-    def mbh_from_vdisp(self, vdisp, scatter=False):
-        return self._mbh_from_galaxy(vdisp, 'vdisp', forward=True, scatter=scatter)
+    @classmethod
+    def mbh_from_vdisp(cls, vdisp, scatter=False):
+        return cls._mbh_from_galaxy(vdisp, 'vdisp', forward=True, scatter=scatter)
 
-    def mbulge_from_mbh(self, mbh, scatter=False):
+    @classmethod
+    def mbulge_from_mbh(cls, mbh, scatter=False):
         """
 
         Arguments
@@ -145,9 +147,10 @@ class _Galaxy_Blackhole_Relation:
         mbulge : bulge stellar-mass in units of [grams]
 
         """
-        return self._mbh_relation(mbh, 'mbulge', forward=False, scatter=scatter)
+        return cls._mbh_relation(mbh, 'mbulge', forward=False, scatter=scatter)
 
-    def vdisp_from_mbh(self, mbh, scatter=False):
+    @classmethod
+    def vdisp_from_mbh(cls, mbh, scatter=False):
         """
 
         Arguments
@@ -159,11 +162,12 @@ class _Galaxy_Blackhole_Relation:
         vdisp : velocity-dispersion (sigma) in units of [cm/s]
 
         """
-        return self._mbh_relation(mbh, 'vdisp', forward=False, scatter=scatter)
+        return cls._mbh_relation(mbh, 'vdisp', forward=False, scatter=scatter)
 
-    def _mbh_relation(self, vals, relation, forward, scatter):
-        x0, y0, alpha, beta, eps = self._values(relation)
-        scatter = self._parse_scatter(scatter)
+    @classmethod
+    def _mbh_relation(cls, vals, relation, forward, scatter):
+        x0, y0, alpha, beta, eps = cls._values(relation)
+        scatter = cls._parse_scatter(scatter)
         shape = np.shape(vals)
 
         params = [alpha, beta, [0.0, eps]]
@@ -177,18 +181,20 @@ class _Galaxy_Blackhole_Relation:
 
         alpha, beta, eps = params
         if forward:
-            rv = self._forward_relation(vals, x0, y0, alpha, beta, eps)
+            rv = cls._forward_relation(vals, x0, y0, alpha, beta, eps)
         else:
-            rv = self._reverse_relation(vals, x0, y0, alpha, beta, eps)
+            rv = cls._reverse_relation(vals, x0, y0, alpha, beta, eps)
 
         return rv
 
-    def _forward_relation(self, xx, x0, y0, alpha, beta, eps):
+    @staticmethod
+    def _forward_relation(xx, x0, y0, alpha, beta, eps):
         mbh = alpha + beta * np.log10(xx/x0) + eps
         mbh = np.power(10.0, mbh) * y0
         return mbh
 
-    def _reverse_relation(self, yy, x0, y0, alpha, beta, eps):
+    @staticmethod
+    def _reverse_relation(yy, x0, y0, alpha, beta, eps):
         gval = np.log10(yy / y0) - eps - alpha
         gval = x0 * np.power(10.0, gval / beta)
         return gval
@@ -309,7 +315,7 @@ class Klypin_2016:
     _mass0 = [5.50e+05, 1.00e+05, 2.00e+04, 9.00e+02, 3.00e+02,
               4.20e+01, 1.70e+01, 8.50e+00, 2.00e+00, 3.00e-01]
 
-    _interp = lambda xx, yy: sp.interpolate.interp1d(xx, yy, kind='linear', bounds_error=False, fill_value=np.nan)
+    _interp = lambda xx, yy: sp.interpolate.interp1d(xx, yy, kind='linear', fill_value='extrapolate')
     _zz = np.log10(1 + np.array(_redz))
     _lin_interp_c0 = _interp(_zz, np.log10(_c0))
     _lin_interp_gamma = _interp(_zz, np.log10(_gamma))
@@ -351,6 +357,18 @@ class _Density_Profile(abc.ABC):
         pass
 
     @classmethod
+    def time_dynamical(cls, rads, *args, **kwargs):
+        """Dynamical time, defined as (G M_enc / r^3) ^ -1/2 = r / v_circ
+        """
+        tdyn = rads / cls.velocity_circular(rads, *args, **kwargs)
+        return tdyn
+
+    @abc.abstractmethod
+    def mass(cls, rads, *args, **kwargs):
+        pass
+
+    '''
+    @classmethod
     def mass(cls, rads, *args, **kwargs):
         dens = cls.density(rads, *args, **kwargs)
         yy = 4*np.pi*rads**2 * dens
@@ -358,39 +376,54 @@ class _Density_Profile(abc.ABC):
         m0 = dens[0] * (4.0/3.0) * np.pi * rads[0] ** 3
         mass = np.concatenate([[m0], mass + m0])
         return mass
-
-    @classmethod
-    def time_dynamical(cls, rads, *args, **kwargs):
-        """Dynamical time, defined as (G M_enc / r^3) ^ -1/2 = r / v_circ
-        """
-        tdyn = rads / cls.velocity_circular(rads, *args, **kwargs)
-        return tdyn
+    '''
 
     @classmethod
     def velocity_circular(cls, rads, *args, **kwargs):
-        """Circular velocity, defined as (G M_enc / r) ^ -1/2
+        """Circular velocity, defined as (G M_enc / r) ^ 1/2
         """
         mass = cls.mass(rads, *args, **kwargs)
-        time = NWTG * mass / rads
-        time = time ** -0.5
-        return time
+        velo = NWTG * mass / rads
+        velo = velo ** 0.5
+        return velo
 
 
 class NFW(_Density_Profile):
 
     @classmethod
     def density(cls, rads, mhalo, redz):
-        # Get Halo concentration
-        conc = cls._concentration(mhalo, redz)
-        dens = cls._density(rads, mhalo, conc, redz)
+        """NFW DM Density profile.
+        [NFW-97]
+        """
+        rho_s, rs = cls._nfw_rho_rad(mhalo, redz)
+        dens = rads / rs
+        dens = dens * np.square(1 + dens)
+        dens = rho_s / dens
         return dens
+
+    @classmethod
+    def mass(cls, rads, mhalo, redz):
+        rads, mhalo, redz = np.broadcast_arrays(rads, mhalo, redz)
+        # Get Halo concentration
+        rho_s, rs = cls._nfw_rho_rad(mhalo, redz)
+        # NOTE: Expression causes numerical problems for rads/rs <~ 1e-8
+        # only use proper analytic expression in safe regime ("hi")
+        # use small radius approximation for unsafe regime ("lo")
+        lo = (rads/rs < 1e-6)
+        hi = ~lo
+        xx = (rs[hi] + rads[hi]) / rs[hi]
+        xx = np.log(xx) + 1.0/xx - 1.0
+        mass = np.zeros_like(rads)
+        mass[hi] = 4.0 * np.pi * rho_s[hi] * rs[hi]**3 * xx
+        mass[lo] = 2.0 * np.pi * rho_s[lo] * rads[lo]**2 * rs[lo]
+        return mass
 
     @staticmethod
     def _concentration(mhalo, redz):
         return Klypin_2016.concentration(mhalo, redz)
 
     @classmethod
-    def radius_scale(cls, mhalo, redz):
+    def _nfw_rho_rad(cls, mhalo, redz):
         conc = cls._concentration(mhalo, redz)
         log_c_term = np.log(1 + conc) - conc/(1+conc)
 
@@ -401,29 +434,7 @@ class NFW(_Density_Profile):
         # scale-radius
         rs = mhalo / (4*np.pi*rho_s*log_c_term)
         rs = np.power(rs, 1.0/3.0)
-        return rs
-
-    @staticmethod
-    def _density(rads, mhalo, conc, redz):
-        """NFW DM Density profile.
-
-        [NFW-97]
-        """
-        log_c_term = np.log(1 + conc) - conc/(1+conc)
-
-        # Critical over-density
-        delta_c = (200/3) * (conc**3) / log_c_term
-        # NFW density (*not* the density at the characteristic-radius)
-        rho_s = cosmo.critical_density(redz).cgs.value * delta_c
-        # scale-radius
-        rs = mhalo / (4*np.pi*rho_s*log_c_term)
-        rs = np.power(rs, 1.0/3.0)
-
-        # Calculate NFW profile
-        dens = rads / rs
-        dens = dens * np.square(1 + dens)
-        dens = rho_s / dens
-        return dens
+        return rho_s, rs
 
 
 # =================================================================================================
@@ -457,8 +468,9 @@ class _StellarMass_HaloMass(abc.ABC):
 class _StellarMass_HaloMass_Redshift(_StellarMass_HaloMass):
 
     _REDZ_GRID_EXTR = [0.0, 9.0]
+    _MSTAR_GRID_EXTR = [5.0, 12.0]
 
-    def __init__(self):
+    def __init__(self, extend_nearest=True):
         self._mhalo_grid = np.logspace(*self._MHALO_GRID_EXTR, self._NUM_GRID) * MSOL
         self._redz_grid = np.linspace(*self._REDZ_GRID_EXTR, self._NUM_GRID+2)
         mhalo = self._mhalo_grid[:, np.newaxis]
@@ -475,10 +487,38 @@ class _StellarMass_HaloMass_Redshift(_StellarMass_HaloMass):
         bb, cc = [bc.ravel() for bc in [bb, cc]]
 
         shape = self._mstar.shape
-        xx = np.linspace(aa.min(), aa.max(), shape[0])
+        aextr = [aa.min(), aa.max()]
+        if self._MSTAR_GRID_EXTR is not None:
+            extr = self._MSTAR_GRID_EXTR
+            if extr[0] < aextr[0] or extr[1] > aextr[1]:
+                log.info("using wider range of stellar-mass than calculated from halo-mass grid!")
+                log.info(f"\tmstar(mhalo) = [{aextr[0]:.2e}, {aextr[1]:.2e}]")
+                log.info(f"\tmstar grid   = [{extr[0]:.2e}, {extr[1]:.2e}]")
+
+            aextr = extr
+
+        xx = np.linspace(*aextr, shape[0])
+
+        self._mstar_grid = MSOL * 10.0 ** xx
+        self._mhalo = MSOL * 10.0 ** cc.reshape(self._mstar.shape)
         yy = redz
         xg, yg = np.meshgrid(xx, yy, indexing='ij')
+        self._aa = aa    # NOTE: these are being stored for debugging/diagnostics
+        self._bb = bb    # NOTE: these are being stored for debugging/diagnostics
+        self._cc = cc    # NOTE: these are being stored for debugging/diagnostics
+        # grid = sp.interpolate.LinearNDInterpolator((aa, bb), cc)((xg, yg))
+        # grid = sp.interpolate.NearestNDInterpolator((aa, bb), cc)((xg, yg))
         grid = sp.interpolate.griddata((aa, bb), cc, (xg, yg))
+        bads = ~np.isfinite(grid)
+        if np.any(bads):
+            if extend_nearest:
+                backup = sp.interpolate.NearestNDInterpolator((aa, bb), cc)((xg, yg))
+                grid[bads] = backup[bads]
+            else:
+                log.warning(f"Non-finite values ({utils.frac_str(bads)}) in mhalo interpolation grid!")
+                log.warning("Use `extend_nearest=True` to fill with nearest values.")
+
+        self._grid = grid  # NOTE: these are being stored for debugging/diagnostics
         # second: construct interpolator from grid to arbitrary scatter points
         interp = sp.interpolate.RegularGridInterpolator((xx, yy), grid)
         self._mhalo_from_mstar_redz = interp
@@ -488,16 +528,33 @@ class _StellarMass_HaloMass_Redshift(_StellarMass_HaloMass):
     def stellar_mass(self, mhalo, redz):
         pass
 
-    def halo_mass(self, mstar, redz):
+    def halo_mass(self, mstar, redz, clip=False):
         if (np.ndim(mstar) not in [0, 1]) or np.any(np.shape(mstar) != np.shape(redz)):
             err = f"both `mstar` ({np.shape(mstar)}) and `redz` ({np.shape(redz)}) must be 1D and same length!"
             log.error(err)
             raise ValueError(err)
 
         squeeze = np.isscalar(mstar)
+        mstar = np.log10(mstar/MSOL)
+        if clip:
+            bounds = np.array([self._mstar_grid[0], self._mstar_grid[-1]])
+            bounds = np.log10(bounds / MSOL)
+            idx = (mstar < bounds[0]) | (bounds[1] < mstar)
+            if np.any(idx):
+                log.info(f"clipping {utils.frac_str(idx)} `mstar` values outside bounds ({bounds})!")
+                mstar[idx] = np.clip(mstar[idx], *bounds)
 
-        vals = np.array([np.log10(mstar/MSOL), redz])
-        ynew = MSOL * 10.0 ** self._mhalo_from_mstar_redz(vals.T)
+        vals = np.array([mstar, redz])
+        try:
+            ynew = MSOL * 10.0 ** self._mhalo_from_mstar_redz(vals.T)
+        except ValueError as err:
+            log.error("Interplation (mstar, redz) ==> mhalo failed!")
+            log.error(err)
+            for vv, nn in zip(vals, ['log10(mstar/Msol)', 'redz']):
+                log.error(f"{nn} : {utils.stats(vv)}")
+
+            raise
+
         if squeeze:
             ynew = ynew.squeeze()
 
