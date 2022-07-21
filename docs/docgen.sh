c2dd1cb96b8590ebed52b2e6eeabb9fea1e4b4f4
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-07-21 15:50:47 -0500

    Have 'docgen.sh' script move to the appropriate directory, so it can be called form anywhere

--- docs/docgen.sh
+++ docs/docgen.sh
@@ -1,3 +1,6 @@
+#!/bin/zsh
+cd "$(dirname "$0")"    # move to this file's directory (`holodeck/docs/`)
 make clean
 sphinx-apidoc -f -o ./source/apidoc_modules ../holodeck
 make html
+cd -                    # move back to previous directory
