ac40a846b8ea11b56287d44c4bacb607d1ea7a47
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-08-04 18:04:51 -0500

    TESTS: start evolution tests

---
+++ holodeck/tests/test_evolution.py
@@ -0,0 +1,34 @@
+"""Tests for the :mod:`holodeck.evolution` submodule.
+"""
+
+import numpy as np
+import pytest
+
+import holodeck as holo
+import holodeck.population
+import holodeck.evolution
+
+
+def test_init_generic_evolution():
+    """Check that instantiation of a generic evolution class succeeds and fails as expected.
+    """
+
+    SIZE = 10
+
+    class Good_Pop(holo.population._Population_Discrete):
+
+        def _init(self):
+            self.mass = np.zeros((SIZE, 2))
+            self.sepa = np.zeros(SIZE)
+            self.scafa = np.zeros(SIZE)
+            return
+
+    class Good_Hard(holo.evolution._Hardening):
+
+        def dadt_dedt(self, *args, **kwargs):
+            return 0.0
+
+    pop = Good_Pop()
+    hard = Good_Hard()
+    evo = holo.evolution.Evolution(pop, hard)
+    return
