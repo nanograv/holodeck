142aa688a2c12bb39b2c17fdff3c0311934049e9
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-08-04 18:05:22 -0500

    DOCS: install requirements when running docgen

--- docs/docgen.sh
+++ docs/docgen.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 cd "$(dirname "$0")"    # move to this file's directory (`holodeck/docs/`)
+pip install -r requirements.txt
 make clean
 sphinx-apidoc -f -o ./source/apidoc_modules ../holodeck
 make html
