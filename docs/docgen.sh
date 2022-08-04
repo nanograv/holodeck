33f39e8c6b7b5316575d1a8580e006399915d7ea
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-08-04 15:19:00 -0500

    DOCS: add docs/html/index.html symlink after docs build

--- docs/docgen.sh
+++ docs/docgen.sh
@@ -3,4 +3,5 @@ cd "$(dirname "$0")"    # move to this file's directory (`holodeck/docs/`)
 make clean
 sphinx-apidoc -f -o ./source/apidoc_modules ../holodeck
 make html
+ln -s $PWD/build/html/index.html $PWD/build/readthedocs.html   # create convenience symlink
 cd -                    # move back to previous directory
