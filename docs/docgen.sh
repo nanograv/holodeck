a5fb5a1965b3d215ffa947469eb7d04a91118772
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-08-18 08:49:10 -0400

    DOCS: create symlink to sphinx output

--- docs/docgen.sh
+++ docs/docgen.sh
@@ -1,8 +1,17 @@
 #!/bin/zsh
+SYMLINK_FILE="readthedocs.html"    # relative to script's WD
 cd "$(dirname "$0")"    # move to this file's directory (`holodeck/docs/`)
+
+path_symlink_file=$PWD/$SYMLINK_FILE
+if [ -f "$path_symlink_file" ] ; then
+    rm "$path_symlink_file"
+fi
+
 pip install -r requirements.txt
 make clean
 sphinx-apidoc -e -P -T -f -o ./source/apidoc_modules ../holodeck
 make html
-ln -s $PWD/build/html/index.html $PWD/build/readthedocs.html   # create convenience symlink
-cd -                    # move back to previous directory
+# create convenience symlink
+ln -s $PWD/build/html/index.html $path_symlink_file
+# move back to previous directory
+cd -
