5c66add56d92d5607c216d63dc49d92f41fad06e
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-08-19 11:54:34 -0400

    Cleanup and clarify usage of different types of 'frequency' (gw vs orb; rest vs observer)

--- holodeck/gravwaves.py
+++ holodeck/gravwaves.py
@@ -17,8 +17,8 @@ _CALC_MC_PARS = ['mass', 'sepa', 'dadt', 'scafa', 'eccen']
 
 class Grav_Waves:
 
-    def __init__(self, bin_evo, freqs, nharms=30, nreals=100):
-        self.freqs = freqs
+    def __init__(self, bin_evo, fobs_gw, nharms=30, nreals=100):
+        self.fobs_gw = fobs_gw
         self.nharms = nharms
         self.nreals = nreals
         self._bin_evo = bin_evo
@@ -33,7 +33,7 @@ class GW_Discrete(Grav_Waves):
         return
 
     def emit(self, eccen=None, stats=False, progress=True, nloudest=5):
-        freqs = self.freqs
+        fobs_gw = self.fobs_gw
         nharms = self.nharms
         nreals = self.nreals
         bin_evo = self._bin_evo
@@ -45,26 +45,28 @@ class GW_Discrete(Grav_Waves):
         if eccen not in [True, False]:
             raise ValueError("`eccen` '{}' is invalid!".format(eccen))
 
-        loudest = np.zeros((freqs.size, nloudest, nreals))
-        fore = np.zeros((freqs.size, nreals))
-        back = np.zeros((freqs.size, nreals))
+        loudest = np.zeros((fobs_gw.size, nloudest, nreals))
+        fore = np.zeros((fobs_gw.size, nreals))
+        back = np.zeros((fobs_gw.size, nreals))
+        both = np.zeros((fobs_gw.size, nreals))
 
         if eccen:
             harm_range = range(1, nharms+1)
         else:
             harm_range = [2]
 
-        freq_iter = enumerate(freqs)
-        freq_iter = utils.tqdm(freq_iter, total=len(freqs), desc='GW frequencies') if progress else freq_iter
-        for ii, fobs in freq_iter:
-            _fore, _back, _loud = _calc_mc_at_fobs(
-                fobs, harm_range, nreals, bin_evo, box_vol,
-                loudest=nloudest
+        freq_iter = enumerate(fobs_gw)
+        freq_iter = utils.tqdm(freq_iter, total=len(fobs_gw), desc='GW frequencies') if progress else freq_iter
+        for ii, fogw in freq_iter:
+            _both, _fore, _back, _loud = _calc_mc_at_fobs(
+                fogw, harm_range, nreals, bin_evo, box_vol, loudest=nloudest
             )
             loudest[ii, :] = _loud
+            both[ii, :] = _both
             fore[ii, :] = _fore
             back[ii, :] = _back
 
+        self.both = np.sqrt(both)
         self.fore = np.sqrt(fore)
         self.back = np.sqrt(back)
         self.strain = np.sqrt(back + fore)
@@ -75,7 +77,7 @@ class GW_Discrete(Grav_Waves):
 class GW_Continuous(Grav_Waves):
 
     def emit(self, eccen=None, stats=False, progress=True, nloudest=5):
-        freqs = self.freqs
+        fobs_gw = self.fobs_gw
         bin_evo = self._bin_evo
         pop = bin_evo._pop
         weight = pop.weight
@@ -94,7 +96,7 @@ class GW_Continuous(Grav_Waves):
         dzdt = H0 * cosmo.efunc(redz) * np.square(1.0 + redz)  # (N,)
 
         # ==> shape (F,N)
-        frest = freqs[:, np.newaxis] / (1.0 + redz[np.newaxis, :])   # rest-frame GW frequency
+        frest = fobs_gw[:, np.newaxis] / (1.0 + redz[np.newaxis, :])   # rest-frame GW frequency
         temp, _ = utils.gw_hardening_rate_dfdt(m1, m2, frest)
         dtr_dlnfr = frest / temp
         # Calculate source-strain for each source (h;  NOT characteristic strain)
@@ -113,29 +115,32 @@ class GW_Continuous(Grav_Waves):
         return
 
 
-def _calc_mc_at_fobs(fobs, harm_range, nreals, bin_evo, box_vol, loudest=5):
+def _calc_mc_at_fobs(fobs_gw, harm_range, nreals, bin_evo, box_vol, loudest=5):
     """
+
+    fobs_gw : float
+        Observer-frame GW-frequency in units of [1/sec].  This is a single, float value.
+
     """
 
     # ---- Interpolate data to all harmonics of this frequency
     harm_range = np.asarray(harm_range)
+    # (H,) observer-frame orbital-frequency for each harmonic
+    fobs_orb = fobs_gw / harm_range
     # Each parameter will be (N, H) = (binaries, harmonics)
-    data_harms = bin_evo.at('fobs', fobs / harm_range, params=_CALC_MC_PARS)
+    data_harms = bin_evo.at('fobs', fobs_orb, params=_CALC_MC_PARS)
 
     # Only examine binaries reaching the given locations before redshift zero (other redz=inifinite)
     redz = data_harms['scafa']
     redz = cosmo.a_to_z(redz)
-    valid = np.isfinite(redz) & (redz > 0.0)
+    valid = (redz > 0.0)
 
     # Broadcast harmonics numbers to correct shape
     harms = np.ones_like(redz, dtype=int) * harm_range[np.newaxis, :]
-    # Select the elements corresponding to the n=2 (circular) harmonic, to use later
-    sel_n2 = np.zeros_like(redz, dtype=bool)
-    sel_n2[(harms == 2)] = 1
     # Select only the valid elements, also converts to 1D, i.e. (N, H) ==> (V,)
-    sel_n2 = sel_n2[valid]
     harms = harms[valid]
     redz = redz[valid]
+
     # If there are eccentricities, calculate the freq-dist-function
     eccen = data_harms['eccen']
     if eccen is None:
@@ -143,8 +148,11 @@ def _calc_mc_at_fobs(fobs, harm_range, nreals, bin_evo, box_vol, loudest=5):
     else:
         gne = utils.gw_freq_dist_func(harms, ee=eccen[valid])
         # BUG: FIX: NOTE: this fails for zero eccentricities (at times?) fix manually!
-        # sel_e0 = (eccen[valid] == 0.0)
         sel_e0 = (eccen[valid] < 1e-8)
+        # Select the elements corresponding to the n=2 (circular) harmonic, to use later
+        sel_n2 = np.zeros_like(redz, dtype=bool)
+        sel_n2[(harms == 2)] = 1
+        sel_n2 = sel_n2[valid]
         gne[sel_e0] = 0.0
         gne[sel_n2 & sel_e0] = 1.0
 
@@ -152,25 +160,27 @@ def _calc_mc_at_fobs(fobs, harm_range, nreals, bin_evo, box_vol, loudest=5):
     # dlum = cosmo.z_to_dlum(redz)
     dcom = cosmo.z_to_dcom(redz)
     zp1 = redz + 1
-    frst_orb = fobs * zp1 / harms
+    frst_orb = fobs_gw * zp1 / harms
     mchirp = data_harms['mass'][valid]
     mchirp = utils.chirp_mass(*mchirp.T)
     dfdt, _ = utils.dfdt_from_dadt(data_harms['dadt'][valid], data_harms['sepa'][valid], freq_orb=frst_orb)
-    _tres = frst_orb / dfdt
+    tfac = frst_orb / dfdt
 
     # Calculate strains from each source
     # hs2 = utils.gw_strain_source(mchirp, dlum, frst_orb)**2
     hs2 = utils.gw_strain_source(mchirp, dcom, frst_orb)**2
+
     # Calculate resampling factors
-    # vfac = 4.0*np.pi*SPLC * dlum**2 / box_vol   # * thub
-    vfac = 4.0*np.pi*SPLC * zp1 * dcom**2 / box_vol   # * thub
-    tfac = _tres  # / thub
+    # [1/s] this is `(dVc/dz) * (dz/dt) / V_sim`
+    vfac = 4.0*np.pi*SPLC * zp1 * dcom**2 / box_vol
 
     # Calculate weightings
-    #    Sesana+08, Eq.10
-    # num_frac = vfac * tfac * zp1
     num_frac = vfac * tfac
-    num_pois = np.random.poisson(num_frac, (nreals, num_frac.size)).T
+    shape = (num_frac.size, nreals)
+    num_pois = np.random.poisson(num_frac[:, np.newaxis], shape)
+    # num_pois = num_frac[:, np.newaxis] * np.ones(shape)
+    # print(f"{utils.frac_str(np.isfinite(num_frac))=}")
+    # print(f"{utils.stats(num_frac)=}")
 
     # --- Calculate GW Signals
     temp = hs2 * gne * (2.0 / harms)**2
@@ -184,7 +194,6 @@ def _calc_mc_at_fobs(fobs, harm_range, nreals, bin_evo, box_vol, loudest=5):
         loud = np.sort(temp[:, np.newaxis] * (num_pois > 0), axis=0)[::-1, :]
         mc_ecc_fore = loud[0, :]
         loud = loud[:loudest, :]
-
         # mc_circ_fore = np.max(temp[:, np.newaxis] * (num_pois > 0) * sel_n2[:, np.newaxis], axis=0)
     else:
         mc_ecc_fore = np.zeros_like(mc_ecc_both)
@@ -199,4 +208,4 @@ def _calc_mc_at_fobs(fobs, harm_range, nreals, bin_evo, box_vol, loudest=5):
     # mc_circ = [mc_circ_fore, mc_circ_back, mc_circ_both]
 
     # return mc_ecc, mc_circ, sa_ecc, sa_circ, loud
-    return mc_ecc_fore, mc_ecc_back, loud
+    return mc_ecc_both, mc_ecc_fore, mc_ecc_back, loud
