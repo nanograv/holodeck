907cd82b77a88587dd6268897df9def2442855ab
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-08-30 15:43:13 -0700

    Add 'extensions.py'

---
+++ holodeck/extensions.py
@@ -0,0 +1,40 @@
+"""Custom holodeck extensions not supported as part of the core package.
+"""
+
+import holodeck as holo
+from holodeck.constants import MSOL, GYR
+
+
+class Realizer:
+
+    def __init__(self, fobs_orb_edges):
+        pop = holo.population.Pop_Illustris()
+        mod_resamp = holo.population.PM_Resample(resample=2.0)
+        pop.modify(mod_resamp)
+
+        mmbulge = holo.relations.MMBulge_MM2013(mamp=7e8*MSOL)
+        mod_mm13 = holo.population.PM_Mass_Reset(mmbulge, scatter=True)
+        pop.modify(mod_mm13)
+
+        fixed = holo.evolution.Fixed_Time.from_pop(pop, 2.0 * GYR)
+        evo = holo.evolution.Evolution(pop, fixed)
+        evo.evolve()
+
+        self._pop = pop
+        self._evo = evo
+
+        names, vals, weights = evo._sample_universe__at_values_weights(fobs_orb_edges)
+        self._fobs_orb_edges = fobs_orb_edges
+        self._vals_names = names
+        self._binary_vals = vals
+        self._binary_weights = weights
+
+    def __call__(self, down_sample=None):
+        evo = self._evo
+
+        fobs_orb_edges = self._fobs_orb_edges
+        names = self._vals_names
+        vals = self._binary_vals
+        weights = self._binary_weights
+        samples = evo._sample_universe__resample(fobs_orb_edges, vals, weights, down_sample)
+        return names, samples
