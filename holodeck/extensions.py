1c298cff5a8c62fa5a4105499b5b464db1727b1e
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-09-08 16:36:47 -0500

    Minor tweaks

--- holodeck/extensions.py
+++ holodeck/extensions.py
@@ -7,16 +7,22 @@ from holodeck.constants import MSOL, GYR
 
 class Realizer:
 
-    def __init__(self, fobs_orb_edges):
+    def __init__(self, fobs_orb_edges, resample=None, lifetime=2*GYR, **mmbulge_kwargs):
         pop = holo.population.Pop_Illustris()
-        mod_resamp = holo.population.PM_Resample(resample=2.0)
-        pop.modify(mod_resamp)
+        if resample is not None:
+            mod_resamp = holo.population.PM_Resample(resample=resample)
+            pop.modify(mod_resamp)
 
-        mmbulge = holo.relations.MMBulge_MM2013(mamp=7e8*MSOL)
+        # mmbulge = holo.relations.MMBulge_KH2013(mamp=7e8*MSOL)
+        mmbulge = holo.relations.MMBulge_KH2013(**mmbulge_kwargs)
         mod_mm13 = holo.population.PM_Mass_Reset(mmbulge, scatter=True)
+        mt, _ = holo.utils.mtmr_from_m1m2(pop.mass)
+        print("mass bef = ", holo.utils.stats(mt/MSOL))
         pop.modify(mod_mm13)
+        mt, _ = holo.utils.mtmr_from_m1m2(pop.mass)
+        print("mass aft = ", holo.utils.stats(mt/MSOL))
 
-        fixed = holo.evolution.Fixed_Time.from_pop(pop, 2.0 * GYR)
+        fixed = holo.evolution.Fixed_Time.from_pop(pop, lifetime)
         evo = holo.evolution.Evolution(pop, fixed)
         evo.evolve()
 
@@ -28,6 +34,7 @@ class Realizer:
         self._vals_names = names
         self._binary_vals = vals
         self._binary_weights = weights
+        return
 
     def __call__(self, down_sample=None):
         evo = self._evo
