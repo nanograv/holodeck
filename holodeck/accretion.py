a128d4213901b12e290ea5beb8bc4ac5e31a0212
Magdalena Siwek <magdalena.siwek@cfa.harvard.edu>
2022-10-17 15:13:09 -0500

    binary eccentricity is now a variable

--- holodeck/accretion.py
+++ holodeck/accretion.py
@@ -1,6 +1,9 @@
 """ MBHB ACCRETION MODELS TO EVOLVE INDIVIDUAL MBH MASSES USING ILLUSTRIS ACCRETION RATES """
 import numpy as np
 import os
+import random
+import holodeck as holo
+from scipy import interpolate
 
 class Accretion:
     """
@@ -9,17 +12,20 @@ class Accretion:
     ----------------
 
     """
-    def __init__(self, accmod = 'Basic', f_edd = 0.01, mdot_ext=None, **kwargs):
+    def __init__(self, accmod = 'Basic', f_edd = 0.01, mdot_ext=None, eccen = 0, subpc=True, **kwargs):
         """ First sum masses to get total mass of MBHB """
         self.accmod = accmod
         self.f_edd = f_edd
         self.mdot_ext = mdot_ext
+        self.eccen = eccen
+        self.subpc = subpc
 
     def mdot_eddington(self, mass):
         """ Calculate the total accretion rate based on masses and a
         fraction of the Eddington limit.
+        UNITS = g/s ?
         """
-        from holodeck.constants import SIGMA_T, MPRT, NWTG, MSOL, SPLC, EDDT
+        from holodeck.constants import SIGMA_T, MPRT, NWTG, MSOL, SPLC, EDDT, PC
         #choose radiative efficiency epsilon = 0.1
         #eps = 0.1
         #medd = (4.*np.pi*NWTG*MPRT)/(eps*SPLC*SIGMA_T) * self.mtot
@@ -30,20 +36,37 @@ class Accretion:
     def pref_acc(self, mdot, evol, step):
         """ Choose one of the below models to calculate primary vs secondary accretion rates
             We also supply the instance of the evolution class here in case we need to access eccentricities """
+        from holodeck.constants import SIGMA_T, MPRT, NWTG, MSOL, SPLC, EDDT, PC
         m1 = evol.mass[:, step-1, 0]
         m2 = evol.mass[:, step-1, 1]
 
+        primary_inds_m1 = m1 >= m2
+        primary_inds_m2 = m2 > m1
+        secondary_inds_m1 = m1 <= m2
+        secondary_inds_m2 = m2 < m1
+
+        primary = np.zeros(np.shape(m1))
+        secondary = np.zeros(np.shape(m2))
+
+        primary[primary_inds_m1] = m1[primary_inds_m1]
+        primary[primary_inds_m2] = m2[primary_inds_m2]
+        secondary[secondary_inds_m1] = m1[secondary_inds_m1]
+        secondary[secondary_inds_m2] = m2[secondary_inds_m2]
+
         if self.accmod == 'Siwek22':
-            q_b = m2/m1
+            q_b = secondary/primary
+            q_b_inds = q_b > 1
+            q_b[q_b_inds] = 1./q_b[q_b_inds]
             #if evol has eccen, then do below, if not, set e_b = 0.
             #e_b = evol.eccen[:, step-1]
-            e_b = 0.0
+            e_b = self.eccen#random.uniform(0.1, 1.0)
+            print("e_b = ", e_b)
             """ Now interpolate to get lambda at [q,e] """
             def lambda_qe_interp_2d(fp="data/preferential_accretion/siwek+22/", es=[0.0,0.2,0.4,0.6,0.8]):
                 all_lambdas = []
                 for e in es:
                     fname = 'preferential_accretion/siwek+22/lambda_e=%.2f.txt' %e
-                    fname = os.path.join(_PATH_DATA, fname)
+                    fname = os.path.join(holo._PATH_DATA, fname)
                     lambda_e = np.loadtxt(fname)
                     qs = lambda_e[:,0]
                     lambdas = lambda_e[:,1]
@@ -60,10 +83,28 @@ class Accretion:
 
             lamb_interp = lambda_qe_interp_2d()
             lamb_qe = lamb_interp(q_b, e_b)
-            print("q = %.3f, e = %.3f, lamb_qe = %.3f" %(q_b, e_b, lamb_qe))
-            mdot_1 = 1./(lamb_interp + 1.) * mdot
-            mdot_2 = lamb_interp/(lamb_interp + 1.) * mdot
-            mdot_arr = np.array([mdot_1, mdot_2]).T
+            mdot_1 = 1./(lamb_qe + 1.) * mdot
+            mdot_2 = lamb_qe/(lamb_qe + 1.) * mdot
+
+            mdot_1_arr = np.zeros(np.shape(mdot_1))
+            mdot_2_arr = np.zeros(np.shape(mdot_2))
+
+            mdot_1_arr[primary_inds_m1] = mdot_1[primary_inds_m1]
+            mdot_1_arr[secondary_inds_m1] = mdot_2[secondary_inds_m1]
+
+            mdot_2_arr[secondary_inds_m2] = mdot_2[secondary_inds_m2]
+            mdot_2_arr[primary_inds_m2] = mdot_1[primary_inds_m2]
+
+            """ ONLY PASS CBD ACCRETION RATES TO BINARY IF SEPARATION IS <= PARSEC,
+                OTHERWISE ACCRETION RATES ARE ZERO """
+            if self.subpc:
+                ind_above_pc = evol.sepa[:,step-1] > PC
+                mdot_1_arr[ind_above_pc] = 0.0
+                mdot_2_arr[ind_above_pc] = 0.0
+            """ ONLY PASS CBD ACCRETION RATES TO BINARY IF SEPARATION IS <= PARSEC,
+                OTHERWISE ACCRETION RATES ARE ZERO """
+
+            mdot_arr = np.array([mdot_1_arr, mdot_2_arr]).T
             return(mdot_arr)
 
 
