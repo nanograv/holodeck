BUG: do not force poisson realization values to be ints as this can lead to overflow errors (negative values)

holodeck/gravwaves.py
@@ -439,10 +439,12 @@ def _gws_from_number_grid_integrated(edges, number, realize, sum=True):
     elif utils.isinteger(realize):
         shape = number.shape + (realize,)
         _num = number[..., np.newaxis] * np.ones(shape)
-        hc = hc[..., np.newaxis] * poisson_as_needed(_num)
+        _pnum = poisson_as_needed(_num)
+        hc = hc[..., np.newaxis] * _pnum
         if holo.sam._DEBUG:
             log.info(f"number = {utils.stats(number)}")
             log.info(f"_num = {utils.stats(_num)}")
+            log.info(f"_pnum = {utils.stats(_pnum)}")
             log.info(f"hc = {utils.stats(hc)}")
             holo.sam._check_bads(edges + [np.arange(realize),], hc, "hc")
 
@@ -526,11 +528,14 @@ def poisson_as_needed(values, thresh=1e10):
         Same shape as input `values`.
 
     """
-    output = np.zeros_like(values, dtype=int)
+    # NOTE: do not use `int` type as it can cause overflow errors
+    # output = np.zeros_like(values, dtype=int)
+    output = np.zeros_like(values)
     idx = (values <= thresh)
     output[idx] = np.random.poisson(values[idx])
     tt = values[~idx]
-    output[~idx] = np.floor(np.random.normal(tt, np.sqrt(tt))).astype(int)
+    # output[~idx] = np.floor(np.random.normal(tt, np.sqrt(tt))).astype(int)
+    output[~idx] = np.floor(np.random.normal(tt, np.sqrt(tt)))
     return output
 
 

holodeck/sam.py
@@ -964,11 +964,8 @@ class Semi_Analytic_Model:
         edges[-1] = fobs_orb_edges
         log.debug(f"dnum: {utils.stats(dnum)}")
 
-        # if _DEBUG and np.any(np.isnan(dnum)):
-        if _DEBUG and np.any(~np.isfinite(dnum) | (dnum < 0.0)):
-            err = "Found nan `dnum` values!"
-            log.exception(err)
-            raise ValueError(err)
+        if _DEBUG:
+            _check_bads(edges, dnum, "dnum")
 
         # "integrate" within each bin (i.e. multiply by bin volume)
         # NOTE: `freq` should also be integrated to get proper poisson sampling!
@@ -980,11 +977,6 @@ class Semi_Analytic_Model:
         log.debug(f"number: {utils.stats(number)}")
         log.debug(f"number.sum(): {number.sum():.4e}")
 
-        # if _DEBUG and np.any(~np.isfinite(number) | (number < 0.0)):
-        #     print(f"{np.any(np.isnan(dnum))=}")
-        #     err = "Found nan `number` values!"
-        #     log.exception(err)
-        #     raise ValueError(err)
         if _DEBUG:
             _check_bads(edges, number, "number")
 
@@ -1410,7 +1402,7 @@ def _check_bads(edges, vals, name):
     if not np.any(bads):
         return
 
-    err = f"Found {utils.frac_str(bads)} bad {name} values!"
+    err = f"Found {utils.frac_str(bads)} bad '{name}' values!"
 
     bads = np.where(bads)
     assert len(bads) == len(edges), f"`bads` ({len(bads)=}) does not match `edges` ({len(edges)=}) !"