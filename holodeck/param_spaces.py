cb654dac70b3f4069d694cb210a47d0713d3535d
Luke Zoltan Kelley <lzkelley@gmail.com>
2023-03-09 18:24:19 -0800

    Minor tweaks

--- holodeck/param_spaces.py
+++ holodeck/param_spaces.py
@@ -284,7 +284,7 @@ class PS_Astro_Tight_02(PS_Broad_Uniform_01):
             hard_time=PD_Uniform(0.2, 10.0),   # [Gyr]
             hard_gamma_inner=PD_Uniform(-1.5, -0.5),
 
-            # from `sam-parameters.ipynb` fits to [Tomczak+2014] with 4x stdev values
+            # from `sam-parameters.ipynb` fits to [Tomczak+2014] with 1x stdev values
             gsmf_phi0=PD_Normal(-2.56, 0.1),
             gsmf_mchar0_log10=PD_Normal(10.9, 0.1),   # [log10(Msol)]
             gsmf_alpha0=PD_Normal(-1.2, 0.05),
