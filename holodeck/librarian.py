a6ad7707bc428978efbb636414c2008a9d63f817
Luke Zoltan Kelley <lzkelley@gmail.com>
2023-05-19 13:47:11 -0700

    Parallelize fitting of libraries, cause its kinda slow

--- holodeck/librarian.py
+++ holodeck/librarian.py
@@ -26,6 +26,13 @@ import holodeck.sam_cython
 from holodeck import utils, cosmo
 from holodeck.constants import YR
 
+try:
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+except Exception as err:
+    comm = None
+    holo.log.warning(f"failed to load `mpi4py` in {__file__}: {err}")
+
 
 __version__ = "0.3.2"
 
@@ -857,61 +864,160 @@ def _load_library_from_all_files(path_sims, gwb, hc_ss, hc_bg, sspar, bgpar, log
 
 
 def fit_library_spectra(library_path, log, recreate=False):
-    """
+    """Calculate line fits to library spectra using MPI.
     """
 
-    log.info(f"Fitting library from path {library_path}")
+    # make sure MPI is working
+    _check_mpi_comm(name="fit_library_spectra()")
+
 
     # ---- setup path
 
-    library_path = Path(library_path)
-    if library_path.is_dir():
-        library_path = library_path.joinpath("sam_lib.hdf5")
-    if not library_path.exists() or not library_path.is_file():
-        err = f"{library_path=} must point to an existing library file!"
-        log.exception(err)
-        raise FileNotFoundError(err)
+    if comm.rank == 0:
 
-    log.debug(f"library path = {library_path}")
+        log.info(f"Fitting library from path {library_path}")
 
-    # ---- check for existing fits file
+        library_path = Path(library_path)
+        if library_path.is_dir():
+            library_path = library_path.joinpath("sam_lib.hdf5")
+        if not library_path.exists() or not library_path.is_file():
+            err = f"{library_path=} must point to an existing library file!"
+            log.exception(err)
+            raise FileNotFoundError(err)
 
-    fits_path = get_fits_path(library_path)
-    if fits_path.exists():
-        lvl = log.INFO if recreate else log.WARNING
-        log.log(lvl, f"library fits already exists: {fits_path}")
-        if not recreate:
-            return
+        log.debug(f"library path = {library_path}")
 
-        log.log(lvl, "re-fitting data into new file")
+        # ---- check for existing fits file
 
-    # ---- load library GWB and convert to PSD
+        fits_path = get_fits_path(library_path)
+        return_flag = False
+        if fits_path.exists():
+            lvl = log.INFO if recreate else log.WARNING
+            log.log(lvl, f"library fits already exists: {fits_path}")
+            if recreate:
+                log.log(lvl, "re-fitting data into new file")
+            else:
+                return_flag = True
 
-    with h5py.File(library_path, 'r') as library:
-        fobs = library['fobs'][()]
-        psd = utils.char_strain_to_psd(fobs[np.newaxis, :, np.newaxis], library['gwb'][()])
+        # ---- load library GWB and convert to PSD
 
-    nsamps, nfreqs, nreals = psd.shape
-    log.debug(f"{nsamps=}, {nfreqs=}, {nreals=}")
+        with h5py.File(library_path, 'r') as library:
+            fobs = library['fobs'][()]
+            psd = utils.char_strain_to_psd(fobs[np.newaxis, :, np.newaxis], library['gwb'][()])
+
+        nsamps, nfreqs, nreals = psd.shape
+        log.debug(f"{nsamps=}, {nfreqs=}, {nreals=}")
+
+        # make a copy of the `psd` in the current shape, so that we can confirm shape manipulations work later on
+        psd_check = psd.copy()
+
+        # ---- reshape PSD into (N, F) and we will split the N points across all processors
+
+        # (S, F, R)  ==>  (S, R, F)
+        psd = np.moveaxis(psd, -1, 1)
+        # (S, R, F)  ==>  (S*R, F)
+        psd = psd.reshape((-1, nfreqs))
+
+        # total number of spectra that will be fit
+        ntot = psd.shape[0]
+        indices = range(ntot)
+        indices = np.random.permutation(indices)
+        indices = np.array_split(indices, comm.size)
+        num_ind_per_proc = [len(ii) for ii in indices]
+        log.info(f"{ntot=} cores={comm.size} || max runs per core = {np.max(num_ind_per_proc)}")
+
+    else:
+        fobs = None
+        psd = None
+        nsamps = None
+        nfreqs = None
+        nreals = None
+        indices = None
+        return_flag = None
+
+    # exit if we're not recreating an existing fits file
+    return_flag = comm.bcast(return_flag, root=0)
+    if return_flag:
+        return
+
+    # distribute quantities to all tasks
+    fobs = comm.bcast(fobs, root=0)
+    psd = comm.bcast(psd, root=0)
+    indices = comm.scatter(indices, root=0)
+    comm.barrier()
+
+    # select the PSD spectra for each task
+    my_psd = psd[indices]
+    # log.info(f"{my_psd.shape=}")
 
     # ---- Run fits
 
-    nbins_plaw, fits_plaw = holo.librarian.fit_spectra_plaw(fobs, psd[:10], nbins_list=FITS_NBINS_PLAW)
-    fails = np.any(~np.isfinite(fits_plaw), axis=-1)
-    lvl = log.DEBUG if np.any(fails) else log.INFO
-    log.log(lvl, f"Failed to fit {utils.frac_str(fails)} spectra with powerlaw model")
+    nbins_plaw, fits_plaw = holo.librarian.fit_spectra_plaw(fobs, my_psd, nbins_list=FITS_NBINS_PLAW)
+    nbins_turn, fits_turn = holo.librarian.fit_spectra_turn(fobs, my_psd, nbins_list=FITS_NBINS_TURN)
 
-    nbins_turn, fits_turn = holo.librarian.fit_spectra_turn(fobs, psd[:10], nbins_list=FITS_NBINS_TURN)
-    fails = np.any(~np.isfinite(fits_turn), axis=-1)
-    lvl = log.DEBUG if np.any(fails) else log.INFO
-    log.log(lvl, f"Failed to fit {utils.frac_str(fails)} spectra with turnover model")
+    # ---- gather results and save to output
 
-    # ---- Save to output
+    comm.barrier()
+    all_indices = comm.gather(indices, root=0)
+    all_fits_plaw = comm.gather(fits_plaw, root=0)
+    all_fits_turn = comm.gather(fits_turn, root=0)
+    all_psd = comm.gather(my_psd, root=0)
 
-    np.savez(fits_path, fobs=fobs, psd=psd, nbins_plaw=nbins_plaw, fits_plaw=fits_plaw, nbins_turn=nbins_turn, version=__version__)
-    log.warning(f"Saved fits to {fits_path} size: {utils.get_file_size(fits_path)}")
+    if comm.rank == 0:
 
-    return fits_path
+        # recombine the scatter indices so that we can sort back to the original order of PSD entries
+        indices = np.concatenate(all_indices)
+        # find the ordering to sort indices
+        idx = np.argsort(indices)
+
+        # re-combine all of the separate arrays, [(N1, ...), (N2, ...), ...]  ===>  (N1*N2*etc, ...)
+        fits_plaw = np.concatenate(all_fits_plaw, axis=0)
+        fits_turn = np.concatenate(all_fits_turn, axis=0)
+        all_psd = np.concatenate(all_psd, axis=0)
+
+        # return elements to original order, to match original GWB/PSD
+        fits_plaw = fits_plaw[idx]
+        fits_turn = fits_turn[idx]
+        all_psd = all_psd[idx]
+
+        # confirm that the resorting worked correctly
+        assert np.all(all_psd == psd)
+
+        # reshape arrays to convert back to (Samples, Realizations, ...)
+        len_nbins_plaw = len(nbins_plaw)
+        len_nbins_turn = len(nbins_turn)
+        npars_plaw = np.shape(fits_plaw)[-1]
+        npars_turn = np.shape(fits_turn)[-1]
+        # (S*R, B, P)  ==>  (S, R, B, P)
+        fits_plaw = fits_plaw.reshape(nsamps, nreals, len_nbins_plaw, npars_plaw)
+        fits_turn = fits_turn.reshape(nsamps, nreals, len_nbins_turn, npars_turn)
+        # (S*R, F)  ==>  (S, R, F)
+        all_psd = all_psd.reshape(nsamps, nreals, nfreqs)
+        # (S, R, F)  ==>  (S, F, R)
+        all_psd = np.moveaxis(all_psd, 1, -1)
+
+        # confirm that reshaping worked correctly
+        assert np.all(all_psd == psd_check)
+
+        # Report how many fits failed
+        fails = np.any(~np.isfinite(fits_plaw), axis=-1)
+        lvl = log.INFO if np.any(fails) else log.DEBUG
+        log.log(lvl, f"Failed to fit {utils.frac_str(fails)} spectra with power-law model")
+
+        fails = np.any(~np.isfinite(fits_turn), axis=-1)
+        lvl = log.INFO if np.any(fails) else log.DEBUG
+        log.log(lvl, f"Failed to fit {utils.frac_str(fails)} spectra with turn-over model")
+
+        # --- Save to output file
+
+        np.savez(
+            fits_path, fobs=fobs, psd=psd, version=__version__,
+            nbins_plaw=nbins_plaw, fits_plaw=fits_plaw,
+            nbins_turn=nbins_turn, fits_turn=fits_turn,
+        )
+        log.warning(f"Saved fits to {fits_path} size: {utils.get_file_size(fits_path)}")
+
+    return
 
 
 def fit_all_libraries_in_path(path, log, pattern=None, recreate=False):
@@ -922,7 +1028,7 @@ def fit_all_libraries_in_path(path, log, pattern=None, recreate=False):
     log.info(f"fitting all libraries in path {path}" + msg)
     sub_paths = _find_sam_lib_in_path_tree(path, pattern=pattern)
     log.info(f"found {len(sub_paths)} sam_lib files")
-    for pp in tqdm.tqdm(sub_paths):
+    for pp in sub_paths:
         log.info(f"path: {pp}")
         fit_library_spectra(pp, log, recreate=recreate)
 
@@ -960,8 +1066,8 @@ def _find_sam_lib_in_path_tree(path, pattern=None):
 
 
 def _fit_spectra(freqs, psd, nbins_list, fit_npars, fit_func):
-    assert np.ndim(psd) == 3
-    nsamps, nfreqs, nreals = np.shape(psd)
+    assert np.ndim(psd) == 2
+    npoints, nfreqs = np.shape(psd)
     assert len(freqs) == nfreqs
     assert np.ndim(nbins_list) == 1
 
@@ -983,18 +1089,18 @@ def _fit_spectra(freqs, psd, nbins_list, fit_npars, fit_func):
         return pars
 
     len_nbins = len(nbins_list)
-    shape_fits = [nsamps, nreals, len_nbins, fit_npars]
+    shape_fits = [npoints, len_nbins, fit_npars]
     fits = np.zeros(shape_fits)
     failures = 0
-    ntot = nsamps * nreals
-    for ss, rr in tqdm.tqdm(np.ndindex((nsamps, nreals)), total=ntot):
-        yy = psd[ss, :, rr]
+    iterator = tqdm.trange(npoints) if (comm is None) or (comm.rank == 0) else range(npoints)
+    for ii in iterator:
+        yy = psd[ii, :]
         for nn, nbin in enumerate(nbins_list):
             if nbin > nfreqs:
                 raise ValueError(f"Cannot fit for {nbin=} bins, data has {nfreqs=} frequencies!")
 
             pars = fit_if_all_finite(freqs[:nbin], yy[:nbin])
-            fits[ss, rr, nn, :] = pars
+            fits[ii, nn, :] = pars
             if not np.isfinite(pars[0]):
                 failures += 1
 
@@ -1362,12 +1468,30 @@ def _setup_log(comm, args):
     return log
 
 
+def _check_mpi_comm(name=None):
+    if comm is None:
+        err = f"MPI is required to run {__file__}!"
+        if name is not None:
+            err += "  {name}"
+        holo.log.exception(err)
+        raise RuntimeError(err)
+    return
+
 # ==============================================================================
 # ====    `main` - Script Entry-Point    ====
 # ==============================================================================
 
 
 if __name__ == "__main__":
+
+    _check_mpi_comm(name=__name__)
+
+    def mpiabort_excepthook(type, value, traceback):
+        sys.__excepthook__(type, value, traceback)
+        comm.Abort()
+        return
+
+    sys.excepthook = mpiabort_excepthook
 
     log = holo.log
 
@@ -1424,3 +1548,5 @@ if __name__ == "__main__":
         sys.exit()
 
     log.debug("done")
+
+    sys.exit(0)
