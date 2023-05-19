57e85aaf3068033200dd10e4289aa99b34ea40e6
Emiko <emikogardiner@berkeley.edu>
2023-05-18 17:45:56 -0700

    Split up integrand functions in detstats.py.

--- ecg-notebooks/detstats_functions/library_scripts/ss16_lib_script.py
+++ ecg-notebooks/detstats_functions/library_scripts/ss16_lib_script.py
@@ -8,9 +8,9 @@ hdf_name = LIB_PATH+'/ss_lib.hdf5'
 print('Hdf file:', hdf_name)
 
 # settings to change
-DS_NUM = '2B'
-NPSRS = 60
-SIGMA = 5e-7
+DS_NUM = '03'
+NPSRS = 67
+SIGMA = 4e-7
 
 NSKIES = 25
 THRESH = 0.5
