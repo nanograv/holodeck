fd5f18c6a7e66235e000af1d0cd16a08077d9d78
Emiko <emikogardiner@berkeley.edu>
2023-06-02 16:11:01 -0700

    Working on analytic anisotroy code.

--- holodeck/anisotropy.py
+++ holodeck/anisotropy.py
@@ -11,7 +11,7 @@ import healpy as hp
 import h5py
 
 import holodeck as holo
-from holodeck import utils, cosmo, log, detstats
+from holodeck import utils, cosmo, log, detstats, plot
 from holodeck.constants import YR
 
 NSIDE = 32
@@ -195,7 +195,7 @@ def lib_anisotropy(lib_path, hc_ref_10yr=HC_REF15_10YR, nbest=100, nreals=50, lm
     output_name = output_dir+'/sph_harm_lmax%d_nside%d_nbest%d.npz' % (lmax, nside, nbest)
     print('Saving npz file: ', output_name)
     np.savez(output_name,
-             nsort=nsort, fidx=fidx, hc_tt=hc_tt, hc_ref=hc_ref, ss_shape=shape,
+             nsort=nsort, fidx=fidx, hc_ref=hc_ref, ss_shape=shape,
          moll_hc_best=moll_hc_best, Cl_best=Cl_best, nside=nside, lmax=lmax, fobs=fobs)
     
 
@@ -263,5 +263,256 @@ def Cl_analytic_from_num(fobs_orb_edges, number, hs, realize = False):
     return C0, Cl
 
 
+def strain_amp_at_bin_edges(edges, redz=None):
+    """ Calculate strain amplitude at bin edges, with final or initial redz.
+    
+    """
+    assert len(edges) == 4
+    assert np.all([np.ndim(ee) == 1 for ee in edges])
+
+    foo = edges[-1]                   #: should be observer-frame orbital-frequencies
+    # df = np.diff(foo)                 #: frequency bin widths
+    fc = kale.utils.midpoints(foo)    #: use frequency-bin centers for strain (more accurate!)
 
 
+    if redz is not None:
+        dc = +np.inf * np.ones_like(redz)
+        sel = (redz > 0.0)
+        dc[sel] = holo.cosmo.comoving_distance(redz[sel]).cgs.value
+    else: 
+        redz = edges[2][np.newaxis,np.newaxis,:,np.newaxis]
+        dc = holo.cosmo.comoving_distance(redz).cgs.value
+
+    # ---- calculate GW strain ----
+    mt = (edges[0])
+    mr = (edges[1])
+    mc = utils.chirp_mass_mtmr(mt[:, np.newaxis], mr[np.newaxis, :])
+    mc = mc[:, :, np.newaxis, np.newaxis]
+    
+    # convert from observer-frame to rest-frame; still using frequency-bin centers
+    fr = utils.frst_from_fobs(fc[np.newaxis, np.newaxis, np.newaxis, :], redz)
+
+    hs_edges = utils.gw_strain_source(mc, dc, fr)
+    return hs_edges
+
+
+def strain_amp_at_bin_centers_redz(edges, redz=None):
+    """ Calculate strain amplitude at bin centers, with final or initial redz.
+    
+    """
+    assert len(edges) == 4
+    assert np.all([np.ndim(ee) == 1 for ee in edges])
+
+    foo = edges[-1]                   #: should be observer-frame orbital-frequencies
+    df = np.diff(foo)                 #: frequency bin widths
+    fc = kale.utils.midpoints(foo)    #: use frequency-bin centers for strain (more accurate!)
+
+    # redshifts are defined across 4D grid, shape (M, Q, Z, Fc)
+    #    where M, Q, Z are edges and Fc is frequency centers
+    # find midpoints of redshifts in M, Q, Z dimensions, to end up with (M-1, Q-1, Z-1, Fc)
+    if redz is not None:
+        for dd in range(3):
+            redz = np.moveaxis(redz, dd, 0)
+            redz = kale.utils.midpoints(redz, axis=0)
+            redz = np.moveaxis(redz, 0, dd)
+        dc = +np.inf * np.ones_like(redz)
+        sel = (redz > 0.0)
+        dc[sel] = holo.cosmo.comoving_distance(redz[sel]).cgs.value
+    else:
+        redz = kale.utils.midpoints(edges[2])[np.newaxis,np.newaxis,:,np.newaxis]
+        dc = holo.cosmo.comoving_distance(redz).cgs.value
+
+
+    # ---- calculate GW strain ----
+    mt = kale.utils.midpoints(edges[0])
+    mr = kale.utils.midpoints(edges[1])
+    mc = utils.chirp_mass_mtmr(mt[:, np.newaxis], mr[np.newaxis, :])
+    mc = mc[:, :, np.newaxis, np.newaxis]
+    
+    # convert from observer-frame to rest-frame; still using frequency-bin centers
+    fr = utils.frst_from_fobs(fc[np.newaxis, np.newaxis, np.newaxis, :], redz)
+
+    hs = utils.gw_strain_source(mc, dc, fr)
+    return hs
+
+
+def Cl_analytic_from_dnum(edges, dnum, redz=None, realize=False):
+    """ Calculate Cl using Eq. (17) of Sato-Polito & Kamionkowski
+    Parameters
+    ----------
+    edges : (F,) 1Darray
+        Observed orbital frequency bin edges
+    dnum : (M,Q,Z,F) NDarray
+        dN / [ dlog10M dq dz dlnf ]
+    hs : (M,Q,Z,F) NDarray
+        Strain amplitude of each M,q,z bin
+    
+    """
+    fobs_orb_edges = edges[-1]
+    fobs_gw_edges = fobs_orb_edges * 2.0
+
+    df = np.diff(fobs_orb_edges)                 #: frequency bin widths
+    fc = kale.utils.midpoints(fobs_orb_edges)    #: use frequency-bin centers for strain (more accurate!)
+
+
+    if realize is False:
+        hs_edges = strain_amp_at_bin_edges(edges, redz)
+
+        # ---- integrate from differential-number to number per bin
+        # integrate over dlog10(M)
+        numh2 = utils.trapz(dnum*hs_edges**2, np.log10(edges[0]), axis=0)
+        # integrate over mass-ratio
+        numh2 = utils.trapz(numh2, edges[1], axis=1)
+        # integrate over redshift
+        numh2 = utils.trapz(numh2, edges[2], axis=2)
+        # times dln(f)
+        numh2 = numh2 * np.diff(np.log(fobs_gw_edges)) 
+
+        # integrate over dlog10(M)
+        numh4 = utils.trapz(dnum*hs_edges**4, np.log10(edges[0]), axis=0)
+        # integrate over mass-ratio
+        numh4 = utils.trapz(numh4, edges[1], axis=1)
+        # integrate over redshift
+        numh4 = utils.trapz(numh4, edges[2], axis=2)
+        # times dln(f)
+        print('numh4:', numh4.shape, 'np.diff(np.log(fobs_gw_edges))', np.diff(np.log(fobs_gw_edges)).shape)
+        numh4 = numh4 * np.diff(np.log(fobs_gw_edges))  # how is this not a shape issue??
+
+    elif utils.isinteger(realize):
+        # add reals axis
+        hs_cents = strain_amp_at_bin_centers_redz(edges, redz)[...,np.newaxis]
+        print('hs_cents:', hs_cents.shape)
+        df = df[:,np.newaxis] 
+        fc = fc[:,np.newaxis] 
+
+    
+        number = holo.sam_cython.integrate_differential_number_3dx1d(edges, dnum)
+        shape = number.shape + (realize,)
+        print('number:', number.shape)
+        number = holo.gravwaves.poisson_as_needed(number[...,np.newaxis] * np.ones(shape))
+        print('number:', number.shape)
+
+        numh2 = number * hs_cents**2 * np.diff(np.log(fobs_gw_edges))[:,np.newaxis] 
+        numh4 = number * hs_cents**4 * np.diff(np.log(fobs_gw_edges))[:,np.newaxis] 
+
+
+
+    else:
+        err = "`realize` ({}) must be one of {{False, integer}}!".format(realize)
+        raise ValueError(err)
+
+    print('numh2:', numh2.shape, 'numh4:', numh4.shape)
+
+
+    # df = fobs_orb_widths[np.newaxis, np.newaxis, np.newaxis, :] # (M,Q,Z,F) NDarray
+    # fc = fobs_orb_cents[np.newaxis, np.newaxis, np.newaxis, :]  # (M,Q,Z,F) NDarray
+
+    delta_term = (
+        fc / (4*np.pi * df) * np.sum(numh2, axis=(0,1,2))
+    )**2
+
+    Cl = (
+        (fc / (4*np.pi*df))**2 * np.sum(numh4, axis=(0,1,2))
+    )
+
+    C0 = Cl + delta_term
+
+    return C0, Cl
+
+######################################################################
+############# Plotting Functions
+######################################################################
+
+
+
+def draw_analytic(ax, Cl, C0, fobs_gw_cents, color='tab:orange', label='Eq. 17 analytic', lw=2):
+    xx = fobs_gw_cents
+    yy = Cl/C0 # (F,)
+    ax.plot(xx, yy, color=color, lw=lw, label=label, linestyle='dashdot')
+
+def draw_reals(ax, Cl_many, C0_many, fobs_gw_cents,  color='tab:orange', label= 'Poisson number/bin realization',
+                show_ci=False, show_reals=True, show_median=False):
+    xx = fobs_gw_cents
+    yy = Cl_many/C0_many # (F,R)
+    if show_median:
+        ax.plot(xx, np.median(yy[:,:], axis=-1), color=color) #, label='median of samples, $l=%d$' % ll)     
+    if show_ci:
+        for pp in [50, 98]:
+            percs = pp/2
+            percs = [50-percs, 50+percs]
+            ax.fill_between(xx, *np.percentile(yy[:,:], percs, axis=-1), color=color, alpha=0.1)
+    if show_reals:
+        rr = 0
+        ax.plot(xx, yy[:,rr], color=color, alpha=0.15, linestyle='-', 
+                label = label)
+        for rr in range(1, np.min([nshow, len(Cl_many[0])])):
+            ax.plot(xx, yy[:,rr], color=color, alpha=0.25, linestyle='-')
+
+def draw_spk(ax, label='SP & K Rough Estimate'):
+    spk_xx= np.array([3.5*10**-9, 1.25*10**-8, 1*10**-7]) /YR
+    spk_yy= np.array([1*10**-5, 1*10**-3, 1*10**-1])
+    ax.plot(spk_xx * YR, spk_yy, label=label, color='limegreen', ls='--')
+
+def draw_bayes(ax, lmax, colors = ['k', 'b', 'r', 'g', 'c', 'm']):
+    xx_Nihan = np.array([2.0, 4.0, 5.9, 7.9, 9.9]) *10**-9 # Hz
+    
+    Cl_nihan = np.array([
+    [0.20216773, 0.14690035, 0.09676646, 0.07453352, 0.05500382, 0.03177427],
+    [0.21201336, 0.14884939, 0.10545698, 0.07734305, 0.05257189, 0.03090662],
+    [0.20840993, 0.14836757, 0.09854803, 0.07205384, 0.05409881, 0.03305785],
+    [0.19788951, 0.15765126, 0.09615489, 0.07475364, 0.0527356 , 0.03113331],
+    [0.20182648, 0.14745265, 0.09681202, 0.0746824 , 0.05503161, 0.0317012 ]])
+    for ll in range(lmax):
+        ax.plot(xx_Nihan, Cl_nihan[:,ll]/Cl_nihan[:,0], 
+                    label = '$l=%d$' % (ll+1), 
+                color=colors[ll], marker='o', ms=8)
+        
+def draw_sim(ax, xx, Cl_best, lmax, nshow, show_ci=True, show_reals=True):
+
+    yy = Cl_best[:,:,:,1:]/Cl_best[:,:,:,0,np.newaxis] # (B,F,R,l)
+    yy = np.median(yy, axis=-1) # (B,F,l) median over realizations
+
+    colors = ['k', 'b', 'r', 'g', 'c', 'm']
+    for ll in range(lmax):
+        ax.plot(xx, np.median(yy[:,:,ll], axis=0), color=colors[ll]) #, label='median of samples, $l=%d$' % ll)
+        if show_ci:
+            for pp in [50, 98]:
+                percs = pp/2
+                percs = [50-percs, 50+percs]
+                ax.fill_between(xx, *np.percentile(yy[:,:,ll], percs, axis=0), alpha=0.1, color=colors[ll])
+        if show_reals:
+            for bb in range(0,nshow):
+                # if ll==0 and bb==0:
+                #     label = "individual best samples, median of realizations"
+                # else: 
+                label=None
+                ax.plot(xx, yy[bb,:,ll], color=colors[ll], linestyle=':', alpha=0.1,
+                                 linewidth=1, label=label)
+
+
+# def plot_ClC0(fobs_gw_cents, spk=True, bayes=True, 
+#               sim=True, Cl_best_sim=None, lmax_sim=None,
+#               analytic=False, Cl_analytic, C0_analytic, label_analytic,
+#               anreals=False, Cl_anreals=None):
+#     fig, ax = plot.figax(xlabel=plot.LABEL_GW_FREQUENCY_HZ, ylabel='$C_{\ell>0}/C_0$')
+#     draw_analytic(ax, Cl, C0, fobs_gw_cents, label='analytic from integrated num')
+#     draw_reals(ax, Cl_many, C0_many, fobs_gw_cents, label=None, color='tab:orange')
+    
+#     draw_analytic(ax, Cl_dnum, C0_dnum, fobs_gw_cents, label='analytic from dnum, hs from z_init', color='deeppink')
+#     draw_reals(ax, Cl_dnum_reals, C0_dnum_reals, fobs_gw_cents, label=None, color='deeppink')
+    
+#     draw_analytic(ax, Cl_redz, C0_redz, fobs_gw_cents, label='analytic from dnum, hs from z_final', color='indigo')
+#     draw_reals(ax, Cl_redz_reals, C0_redz_reals, fobs_gw_cents, label=None, color='indigo')
+    
+#     if spk: draw_spk(ax, label='S-P & K')
+#     if bayes: draw_bayes(ax, lmax=6)
+#     if sim and (Cl_best_sim is not None) and (lmax_sim is not None): 
+#         draw_sim(ax, fobs_gw_cents, Cl_best_sim, lmax_sim, show_ci=True, show_reals=True, nshow=10)
+#     # ax.set_ylim(10**-6, 10**0)
+#     plot._twin_yr(ax, nano=False)
+#     ax.set_xlim(fobs_gw_cents[0]- 10**(-10), 1/YR)
+
+#     fig.legend(bbox_to_anchor=(0,-0.15), loc='upper left', bbox_transform = ax.transAxes, ncols=3)
+#     return fig
+
+# fig = plot_ClC0()  
