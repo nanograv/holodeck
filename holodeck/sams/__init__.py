b2990042843366da60fc678c52cbe9e83e8ca25c
Luke Zoltan Kelley <lzkelley@gmail.com>
2023-06-19 12:28:04 -0700

    Update SAM notebook to include GMR demo

--- holodeck/sams/__init__.py
+++ holodeck/sams/__init__.py
@@ -3,5 +3,5 @@
 
 from . import sam
 from . import comps
-from sam import Semi_Analytic_Model
-from comps import (GSMF_Schechter, GPF_Power_Law, GMT_Power_Law, GMR_Illustris)
+from .sam import Semi_Analytic_Model
+from .comps import (GSMF_Schechter, GPF_Power_Law, GMT_Power_Law, GMR_Illustris)
