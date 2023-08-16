ce13b0f1238a34a323688bf75ac745f216457d13
Emiko C Gardiner <ecg@berkeley.edu>
2023-08-16 10:39:23 -0700

    Change to midvars.

--- ecg-notebooks/parameter_investigation/calvar.sh
+++ ecg-notebooks/parameter_investigation/calvar.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py hard_time --detstats --debug -r 500 -s 100 -v 21 --cv 20
+python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py hard_time --detstats --debug -r 500 -s 100 -v 21 --cv 10
 
-python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py gsmf_phi0 --detstats --debug -r 500 -s 100 -v 21 --cv 0
+python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py gsmf_phi0 --detstats --debug -r 500 -s 100 -v 21 --cv 10
 
-python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py gsmf_mchar0_log10 --detstats --debug -r 500 -s 100 -v 21 --cv 0
+python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py gsmf_mchar0_log10 --detstats --debug -r 500 -s 100 -v 21 --cv 10
 
-python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py mmb_mamp_log10 --detstats --debug -r 500 -s 100 -v 21 --cv 0
+python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py mmb_mamp_log10 --detstats --debug -r 500 -s 100 -v 21 --cv 10
 
-python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py mmb_scatter_dex --detstats --debug -r 500 -s 100 -v 21 --cv 0
+python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py mmb_scatter_dex --detstats --debug -r 500 -s 100 -v 21 --cv 10
 
-python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py hard_gamma_inner --detstats --debug -r 500 -s 100 -v 21 --cv 20
+python ecg-notebooks/parameter_investigation/detect_model_varclbrt.py hard_gamma_inner --detstats --debug -r 500 -s 100 -v 21 --cv 10
