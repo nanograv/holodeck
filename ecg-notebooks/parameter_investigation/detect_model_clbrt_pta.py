a65dd11fc11e729f1858b0b954931d8c0ecd2c17
Emiko C Gardiner <ecg@berkeley.edu>
2023-08-23 11:23:59 -0700

    Remove extraneous print statement.

--- ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py
+++ ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py
@@ -283,7 +283,6 @@ def resample_loudest(hc_ss, hc_bg, nloudest):
     new_hc_bg = np.sqrt(hc_bg**2 + np.sum(hc_ss[...,nloudest:-1]**2, axis=-1))
     new_hc_ss = hc_ss[...,0:nloudest]
 
-    print(new_hc_ss.shape)
     return new_hc_ss, new_hc_bg
 
 
