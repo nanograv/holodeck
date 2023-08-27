d0a73352a13453c560141c358a5e07cf686c6efb
Emiko C Gardiner <ecg@berkeley.edu>
2023-08-26 22:18:40 -0700

    do bgl1 with cv10.

--- ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.sh
+++ ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.sh
@@ -3,5 +3,5 @@
 
 for TARGET in gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex hard_gamma_inner
 do 
-    python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -s 100 -v 21 -l 10 --bgl 1
+    python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -s 100 -v 21 -l 10 --bgl 1 --cv 10
 done
