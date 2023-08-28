5d5af416f02c460228b920d945b75074699dd722
Emiko C Gardiner <ecg@berkeley.edu>
2023-08-27 17:49:27 -0700

    Run detect models with gw-only, bgl1, realization-calibrated.

--- ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.sh
+++ ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.sh
@@ -9,5 +9,5 @@
 # gw only
 for TARGET in gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex 
 do 
-    python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -s 100 -v 21 -l 10 --bgl 1 --cv 10 --gw_only
+    python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -s 100 -v 21 -l 10 --bgl 1 --gw_only
 done
