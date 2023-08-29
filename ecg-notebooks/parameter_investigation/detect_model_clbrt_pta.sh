3b20810d84ae54018a785ac5b8fba2132b2724d7
Emiko C Gardiner <ecg@berkeley.edu>
2023-08-29 09:21:38 -0700

    Run gsc shape with bgl1.

--- ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.sh
+++ ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # redz
-for TARGET in hard_time gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex hard_gamma_inner
+for TARGET in mmb_scatter_dex hard_gamma_inner
 do 
     python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -s 100 -v 21 -l 10 --bgl 1 --gsc
 done
