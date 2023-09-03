eb22e3873d4d325b4ee03d6f5ad5558c5a769bd0
Emiko C Gardiner <ecg@berkeley.edu>
2023-09-02 19:36:47 -0700

    Fix run red noise models.

--- ecg-notebooks/parameter_investigation/detect_model_clbrt_pta_rednoise.sh
+++ ecg-notebooks/parameter_investigation/detect_model_clbrt_pta_rednoise.sh
@@ -5,7 +5,7 @@ for TARGET in hard_time gsmf_phi0
 do 
     for RED_GAMMA in -1.5 -3.0
     do
-        python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --construct --debug -r 500 -v 21 -l 10 --bgl 1 --red2white 1.0 --red_gamma $RED_GAMMA
+        python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -v 21 -l 10 --bgl 1 --red2white 1.0 --red_gamma $RED_GAMMA
     done
 done
 
@@ -15,7 +15,7 @@ do
     do
         for RED2WHITE in 0.5 1.0 2.0
         do
-            python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --construct --debug -r 500 -v 21 -l 10 --bgl 1 --red2white $RED2WHITE --red_gamma $RED_GAMMA
+            python ecg-notebooks/parameter_investigation/detect_model_clbrt_pta.py $TARGET --detstats --debug -r 500 -v 21 -l 10 --bgl 1 --red2white $RED2WHITE --red_gamma $RED_GAMMA
         done
     done
 done
