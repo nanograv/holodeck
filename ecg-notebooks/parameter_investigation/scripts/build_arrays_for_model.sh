f00010db257a1a7365198ee8ab8c7995516e36f2
Emiko C Gardiner <ecg@berkeley.edu>
2023-09-07 11:06:37 -0700

    Run anis_freq for GW only.

--- ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.sh
+++ ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for TARGET in hard_time gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex hard_gamma_inner
+for TARGET in gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex
 do 
-    python ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.py $TARGET --anis_var -r 500 -v 21 -l 10 --bgl 1 
+    python ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.py $TARGET --anis_freq -r 500 -v 3 -l 10 --bgl 1 --gw_only
 
 done
