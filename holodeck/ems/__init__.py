25e6d36112ea4cc285d6502a58a23414e7682013
Luke Zoltan Kelley <lzkelley@gmail.com>
2023-09-06 17:42:41 -0700

    Update SDSS bands using SVO, add LSST bands.  Move notebooks to dev/ subdir

--- holodeck/ems/__init__.py
+++ holodeck/ems/__init__.py
@@ -6,7 +6,8 @@ from . import drw   # noqa
 from . runnoe2012 import Runnoe2012   # noqa
 
 
-bands_sdss = basics.SDSS_Bands()
+bands_sdss = basics.Bands_SDSS()
+bands_lsst = basics.Bands_LSST()
 MacLeod2010 = drw.MacLeod2010
 runnoe2012 = Runnoe2012()
 
