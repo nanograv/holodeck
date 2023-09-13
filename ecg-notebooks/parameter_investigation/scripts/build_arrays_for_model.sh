a709a6ea3a8822d107e7277091717a29b68f5aca
Emiko C Gardiner <ecg@berkeley.edu>
2023-09-12 18:59:12 -0700

    Replot favg with bgl bug fixed.

--- ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.sh
+++ ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+for TARGET in hard_time gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex hard_gamma_inner
+do 
+    python ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.py $TARGET --favg -r 500 -v 21 --bgl 1
+
+done
+
+
 for TARGET in gsmf_phi0 gsmf_mchar0_log10 mmb_mamp_log10 mmb_scatter_dex
 do 
-    python ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.py $TARGET --anis_freq -r 500 -v 3 -l 1000 --gw_only
+    python ecg-notebooks/parameter_investigation/scripts/build_arrays_for_model.py $TARGET --favg -r 500 -v 21 --bgl 1 --gw_only
 
 done
