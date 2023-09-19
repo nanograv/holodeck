576487baa90233a04908afe025f4fe04ba703e5d
Luke Zoltan Kelley <lzkelley@gmail.com>
2023-09-19 18:43:33 +0200

    Update deprecated module call

--- holodeck/hardening.py
+++ holodeck/hardening.py
@@ -1439,7 +1439,7 @@ class Fixed_Time_2PL_SAM(_Hardening):
         shape = mtot.shape
         mt, mr = [mm.flatten() for mm in [mtot, mrat]]
 
-        norm_log10 = holo.sams.cyutils.find_2pwl_hardening_norm(
+        norm_log10 = holo.sams.sam_cyutils.find_2pwl_hardening_norm(
             time, mt, mr,
             sepa_init, rchar, gamma_inner, gamma_outer, num_steps,
         )
@@ -1476,7 +1476,7 @@ class Fixed_Time_2PL_SAM(_Hardening):
         shape = args[0].shape
         args = [aa.flatten() for aa in args]
         mtot, mrat, sepa, norm = args
-        dadt_vals = holo.sams.cyutils.hard_func_2pwl_gw(
+        dadt_vals = holo.sams.sam_cyutils.hard_func_2pwl_gw(
             mtot, mrat, sepa, norm,                             # must all be 1darrays of matching size (X,)
             self._rchar, self._gamma_inner, self._gamma_outer   # must all be scalars
         )
