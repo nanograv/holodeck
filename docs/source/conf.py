7259c91d5996f8e6f55a8ab8157528eb60cd67e4
Luke Zoltan Kelley <lzkelley@gmail.com>
2024-02-07 10:41:14 -0800

    BUG: readthedocs build error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,10 +35,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
 
+# -- Setup Sphinx's API-DOC --------------------------------------------------
 
 # Define the configuration for sphinx-apidoc
 apidoc_module_dir = 'source/apidoc_modules'
