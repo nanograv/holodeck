4d54914c6889ef5ed321dc8865c9d69d5f97bf4a
Luke Zoltan Kelley <lzkelley@gmail.com>
2024-02-09 12:06:47 -0800

    Updates

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../holodeck/'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../holodeck/'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -53,6 +53,7 @@ html_static_path = []
 apidoc_module_dir = 'source/apidoc_modules'
 apidoc_excluded_paths = ['../holodeck/detstats.py', '../holodeck/anisotropy.py']
 apidoc_command_options = ' '.join([
+    '-M',  # put modules before submodules
     '-e',  # Create separate files for each module
     '-P',  # Include private modules
     '-T',  # Include docstrings in the table of contents
@@ -63,9 +64,10 @@ apidoc_command_options = ' '.join([
 def run_apidoc(_):
     from sphinx.ext.apidoc import main
     argv = [
-        '-M',
-        '-T',
-        '-f',
+        # '-M',
+        # '-T',
+        # '-f',
+        *apidoc_command_options,
         '-o', apidoc_module_dir,
         '../holodeck',
         *apidoc_excluded_paths,
