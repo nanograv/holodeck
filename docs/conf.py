3228838253eecfc246fbdc61b150ea51c74f5a6e
Luke Zoltan Kelley <lzkelley@gmail.com>
2024-02-09 11:13:23 -0800

    BUG: TRY AGAIN

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../holodeck/'))
+sys.path.insert(0, os.path.abspath('../holodeck/'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
