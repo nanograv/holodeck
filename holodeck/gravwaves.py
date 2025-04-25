a81cee67013525c8434ac72d13e2a6274b979c4f
Laura Blecha <lblecha@wifi-10-40-176-30.wifi.berkeley.edu>
2025-04-24 17:27:36 -0700

    added binary pars redz_init and redz_final in _gws_harmonics_at_evo_fobs

--- holodeck/gravwaves.py
+++ holodeck/gravwaves.py
@@ -198,6 +198,14 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
     # Each parameter will be (N, H) = (binaries, harmonics)
     data_harms = evo.at('fobs', fobs_orb, params=_CALC_MC_PARS) 
 
+    ## DEBUG - trying to get redz initital and final for each binary
+    ## but need to make it match shape of data_harms
+    redz_init = evo.scafa[:,0]
+    redz_final = evo.scafa[:,-1]
+    redz_init = cosmo.a_to_z(redz_init)
+    redz_final = cosmo.a_to_z(redz_final)
+    print(f"{redz_init.shape=} {redz_final.shape=}")
+    
     # Only examine binaries reaching the given locations before redshift zero (other redz=inifinite)
     # (N, H)
     redz = data_harms['scafa']
@@ -206,6 +214,7 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
 
     redz = cosmo.a_to_z(redz)
     valid = (redz > 0.0)
+    print(f"\nDEBUG: {redz.shape=} {valid.shape=}\n")
     # There are 'V' valid == True elements of the (N, H) arrays, such that V <= N*H
     # anytime an (N, H) ndarray is sliced by the `valid` ndarray, it results in a (V,) ndarray
 
@@ -247,7 +256,9 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
 
     frst_orb = utils.frst_from_fobs(fobs_orb, redz)
     # Select only the valid elements, also converts to 1D, i.e. (N, H) ==> (V,)
+    print(f"DEBUG: before 'valid' mask applied: {redz.shape=}")
     redz = redz[valid]
+    print(f"DEBUG: after 'valid' mask applied: {redz.shape=}")
     frst_orb = frst_orb[valid]
     # Calculate required parameters for valid binaries (V,)
     dcom = cosmo.z_to_dcom(redz)
@@ -259,6 +270,12 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
     mtot, mrat = utils.mtmr_from_m1m2(data_harms['mass'][valid]) ## debug
     mpri = mtot / (1 + mrat) ## debug
     print(f"*** DEBUG *** {mrat.min()=}, {mrat.max()}")
+
+    # DEBUG: adding redz_init and redz_final here
+    # shape mismatch with `valid`, dealing with this by just calling 0 index, 
+    # (shouldnt matter since it's a binary property)
+    redz_init = redz_init[valid[:,0]]
+    redz_final = redz_final[valid[:,0]]
     
     # Calculate strains from each source
     hs2 = utils.gw_strain_source(mchirp, dcom, frst_orb)**2
@@ -296,6 +313,7 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
         #debug_loud = loud[:loudest, :]
         mchirp_loud = np.take_along_axis(mchirp[:, np.newaxis] * (num_pois > 0), idx_loud, axis=0)
         mchirp_loud = mchirp_loud[:loudest,:]
+        print(f"DEBUG: before 'redz_loud' definition: {redz.shape=}")
         redz_loud = np.take_along_axis(redz[:, np.newaxis] * (num_pois > 0), idx_loud, axis=0)
         redz_loud = redz_loud[:loudest,:]
         sepa_loud = np.take_along_axis(sepa[:, np.newaxis] * (num_pois > 0), idx_loud, axis=0)
@@ -304,6 +322,10 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
         mpri_loud = mpri_loud[:loudest,:]
         mrat_loud = np.take_along_axis(mrat[:, np.newaxis] * (num_pois > 0), idx_loud, axis=0)
         mrat_loud = mrat_loud[:loudest,:]
+        redz_init_loud = np.take_along_axis(redz_init[:, np.newaxis] * (num_pois > 0), idx_loud, axis=0)
+        redz_init_loud = redz_init_loud[:loudest,:]
+        redz_final_loud = np.take_along_axis(redz_final[:, np.newaxis] * (num_pois > 0), idx_loud, axis=0)
+        redz_final_loud = redz_final_loud[:loudest,:]
 
         print(f"{mchirp.shape=}, {redz.shape=}, {idx_loud.shape}=")
         print(f"{mchirp_loud.shape=}, {redz_loud.shape=}") #, {idx_loud.shape}=")
@@ -320,6 +342,10 @@ def _gws_harmonics_at_evo_fobs(fobs_gw, dlnf, evo, harm_range, nreals, box_vol, 
         print(f"redz_loud: min={(redz_loud).min():.4g}, max={(redz_loud).max():.4g}, med={np.median(redz_loud):.4g}")
         print(f"sepa: min={(sepa/PC).min():.4g}, max={(sepa/PC).max():.4g}, med={np.median(sepa/PC):.4g}")
         print(f"sepa_loud: min={(sepa_loud/PC).min():.4g}, max={(sepa_loud/PC).max():.4g}, med={np.median(sepa_loud/PC):.4g}")
+        print(f"redz_init: min={(redz_init).min():.4g}, max={(redz_init).max():.4g}, med={np.median(redz_init):.4g}")
+        print(f"redz_init_loud: min={(redz_init_loud).min():.4g}, max={(redz_init_loud).max():.4g}, med={np.median(redz_init_loud):.4g}")
+        print(f"redz_final: min={(redz_final).min():.4g}, max={(redz_final).max():.4g}, med={np.median(redz_final):.4g}")
+        print(f"redz_final_loud: min={(redz_final_loud).min():.4g}, max={(redz_final_loud).max():.4g}, med={np.median(redz_final_loud):.4g}")
         #print(f"{debug_loud.shape=}, {debug_loud=}")
     else:
         fore = np.zeros_like(both)
