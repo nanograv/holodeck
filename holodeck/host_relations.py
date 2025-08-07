816d09d047416f8da4371b6c4a55ba4835bdff03
Cayenne Matt <59581529+CayenneMatt@users.noreply.github.com>
2025-08-07 16:50:13 -0400

    Update host_relations.py
    Added _zplaw as attribute to MMBulge_Standard (set to 0.0)

--- holodeck/host_relations.py
+++ holodeck/host_relations.py
@@ -700,6 +700,7 @@ class MMBulge_Standard(_MMBulge_Relation):
         self._mplaw = mplaw                 #: Mass Power-law index
         self._mref = mref                   #: Reference Mass (argument normalization)
         self._scatter_dex = scatter_dex
+        self._zplaw = 0.0
 
         if len(kwargs) > 0:
             warn = f"Unused parameters passed to {self}!  {kwargs=}"
