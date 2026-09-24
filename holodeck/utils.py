corrected minor inconsistency in nu_inner min calculation; makes negligible difference: added missing -1 term in numerator s.t. criterion is dadt_tot<dadt_max, not dadt_in<dadt_max. affects only get_nuin_min() and allowed_param_range()

holodeck/utils.py
@@ -2630,7 +2630,7 @@ def get_nuin_min(lgr9rg, DEFAULTS, isco_in_rg=6.0, nu_inner_absmin=-4.0, speed_l
         dadt_gw_crit[idx] = holo.utils.gw_hardening_rate_dadt(m1[idx], m2[idx], rgw_crit[idx])
 
         lgrdiff = np.log10(rchar) - np.log10(rgw_crit)
-        nuin_min_calc = 1.0 - (np.log10(speed_limit) - np.log10(-dadt_gw_crit)) / lgrdiff
+        nuin_min_calc = 1.0 - np.log10(speed_limit/np.abs(dadt_gw_crit)-1) / lgrdiff
         nuin_min_calc = np.maximum(nu_inner_absmin, nuin_min_calc)
 
     if np.any(lgrdiff[mask_inner] < 0):